f424f911543b1bbaa22c68860c393829a12b3585
Mendi03 <salva.mendis.03@gmail.com>
2024-12-02 16:06:10 -0600

    combined all js and css files.
    js files and css files have been combined to a single file respectively. Also started making webpage responsive.

--- Website_files/javascript/index.js
+++ Website_files/javascript/index.js
@@ -49,6 +49,36 @@ document.addEventListener('click', e => {
     if (read_btn != null) {
         showText(read_btn)
     }
+
+    // package plan section cards
+
+    let selected_btn
+    if(e.target.matches('#pickup .plan_nav li')){
+        selected_btn = e.target
+    }
+    if(e.target.matches('#on_site .plan_nav li')){
+        selected_btn = e.target
+    }
+    else{
+        selected_btn = e.target.closest('.plan_nav li')
+    }
+    if(selected_btn != null){
+        onCardBtnClick(selected_btn)
+    }
+
+    // FAQ section
+
+    let question
+    if(e.target.matches('.question')){
+        question = e.target
+    }
+    else {
+        question = e.target.closest('.question')
+    }
+    if(question != null){
+        onFAQClick(question)
+    }
+
 })
 
 // ****************************** Index page ******************************
@@ -116,6 +146,141 @@ function showText(read_btn){
     })    
 }
 
+// ****************************** Menu page ******************************
 
+function onCardBtnClick(selected_btn){
+    let card_nav = selected_btn.closest('.plan_nav')
 
+    // this code determines which package plan is being selected by labeling the li item with the class 'selected'
+    for(const child of card_nav.children){
+        if(child != selected_btn){
+            child.classList.remove('selected')
+        }
+        else{
+            if(selected_btn.classList != 'selected')
+            {
+                selected_btn.classList.add('selected')
+                // selected_btn.textContent = 'test'
+            }
+        } 
+    }
 
+    //the following code determines the text content of the selected plan
+    let parent = card_nav.closest('.plan_card')
+
+    let price = parent.querySelector('#price h1');
+    let servings = parent.querySelector('#servings');
+    let entire_list = parent.querySelector('.details');
+    let list_children = entire_list.children;
+    let classes = selected_btn.classList.value;
+    //the following code determines the text content of the selected plan
+
+    // this is an anchor tag with a url that sends customers to our payment gateway
+    let chosen_plan = document.querySelector('#card_btn');
+
+    switch(classes){
+        case "plan1 selected":
+            price.textContent = '600';
+            servings.textContent = 'For 50-60 Servings';
+            list_children[0].textContent = '70 lbs of Carnitas*';
+            list_children[2].textContent = '30 Gorditas';
+            list_children[3].textContent = '30 Crispy Tacos';
+            list_children[4].textContent = '15 Tortas';
+            chosen_plan.setAttribute('href', 'https://www.youtube.com/')
+            break;
+
+        case "plan2 selected":
+            price.textContent = '800';
+            servings.textContent = 'For 100-120 Servings';
+            list_children[0].textContent = '140 lbs of Carnitas*';
+            list_children[2].textContent = '50 Gorditas';
+            list_children[3].textContent = '50 Crispy Tacos';
+            list_children[4].textContent = '20 Tortas';
+            chosen_plan.setAttribute('href', 'https://www.w3schools.com/jquery/jquery_intro.asp')
+            break;
+
+        case 'plan3 selected':
+            price.textContent = '???';
+            servings.textContent = 'For 150-180 Servings';
+            list_children[0].textContent = '210 lbs of Carnitas*';
+            list_children[2].textContent = '75 Gorditas';
+            list_children[3].textContent = '?? Crispy Tacos';
+            list_children[4].textContent = '?? Tortas';
+            chosen_plan.setAttribute('href', 'https://play.hbomax.com/page/urn:hbo:page:home')
+            break;
+   
+        case 'plan4 selected':
+            price.textContent = '???';
+            servings.textContent = 'For 200-240 Servings';
+            list_children[0].textContent = '280 lbs of Carnitas*';
+            list_children[2].textContent = '100 Gorditas';
+            list_children[3].textContent = '?? Crispy Tacos';
+            list_children[4].textContent = '?? Tortas';
+            chosen_plan.setAttribute('href', 'https://www.google.com/webhp?hl=en&sa=X&ved=0ahUKEwidz7bcpoKKAxWmJNAFHYe7JrkQPAgI')
+            break;
+        
+        case "plan5 selected":
+            price.textContent = '1200';
+            servings.textContent = 'For 50-60 Servings';
+            list_children[0].textContent = '70 lbs of Carnitas*';
+            list_children[2].textContent = '30 Gorditas';
+            list_children[3].textContent = '30 Crispy Tacos';
+            list_children[4].textContent = '15 Tortas';
+            chosen_plan.setAttribute('href', 'https://www.youtube.com/')
+            break;
+
+        case "plan6 selected":
+            price.textContent = '1500';
+            servings.textContent = 'For 100-120 Servings';
+            list_children[0].textContent = '140 lbs of Carnitas*';
+            list_children[2].textContent = '50 Gorditas';
+            list_children[3].textContent = '50 Crispy Tacos';
+            list_children[4].textContent = '20 Tortas';
+            chosen_plan.setAttribute('href', 'https://www.w3schools.com/jquery/jquery_intro.asp')
+            break;
+
+        case 'plan7 selected':
+            price.textContent = '2000';
+            servings.textContent = 'For 150-180 Servings';
+            list_children[0].textContent = '210 lbs of Carnitas*';
+            list_children[2].textContent = '75 Gorditas';
+            list_children[3].textContent = '?? Crispy Tacos';
+            list_children[4].textContent = '?? Tortas';
+            chosen_plan.setAttribute('href', 'https://play.hbomax.com/page/urn:hbo:page:home')
+            break;
+   
+        case 'plan8 selected':
+            price.textContent = '2500';
+            servings.textContent = 'For 200-240 Servings';
+            list_children[0].textContent = '280 lbs of Carnitas*';
+            list_children[2].textContent = '100 Gorditas';
+            list_children[3].textContent = '?? Crispy Tacos';
+            list_children[4].textContent = '?? Tortas';
+            chosen_plan.setAttribute('href', 'https://www.google.com/webhp?hl=en&sa=X&ved=0ahUKEwidz7bcpoKKAxWmJNAFHYe7JrkQPAgI')
+            break;
+        
+
+        default:
+            console.log('not_workin');
+    }
+
+}
+
+// ****************************** Contact page ******************************
+
+function onFAQClick(question){
+
+        question.classList.toggle('active');
+        const answer = question.lastElementChild
+        console.log(answer);
+        
+        if(question.classList.contains('active')){
+            answer.style.setProperty('max-height', answer.scrollHeight + 'px');
+            question.style.setProperty('background-image', 'url(../Images/chevron-up.svg)');
+        }
+        else{
+            answer.style.maxHeight = 0;
+            question.style.setProperty('background-image', 'url(../Images/chevron-down.svg)');
+        }
+}
+       
